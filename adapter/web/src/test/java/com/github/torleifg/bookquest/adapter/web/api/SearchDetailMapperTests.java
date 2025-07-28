f8d9187d170cc0e651e05836af9100cb10ccb05d
Torleif Gravelsæter <torgravels@gmail.com>
2025-07-28 23:18:38 +0200

    Bump dependencies (#59)

--- adapter/web/src/test/java/com/github/torleifg/bookquest/adapter/web/api/SearchDetailMapperTests.java
+++ adapter/web/src/test/java/com/github/torleifg/bookquest/adapter/web/api/SearchDetailMapperTests.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class SearchApiMapperTests {
+class SearchDetailMapperTests {
 
     @Mock
     MessageSource languageSource;
